5b94494b8ef8d5f630a77f1821abecaa8c461795
mookim <github@moo.kim>
2026-05-11 15:05:25 +0000

    fix: detect unchanged incremental containers

--- pkg/core/types.go
+++ pkg/core/types.go
@@ -6,34 +6,35 @@ type Service struct {
 	Name          string   `json:"name"`
 	ContainerName string   `json:"container_name"`
 	Image         string   `json:"image"`
+	ConfigHash    string   `json:"config_hash,omitempty"`
 	DependsOn     []string `json:"depends_on"`
 	StartOrder    int      `json:"start_order"`
 	Ports         []string `json:"ports"`
 }
 
 type PackageManifest struct {
-	Version    string    `json:"version"`
-	CreatedAt time.Time `json:"created_at"`
-	Project   string    `json:"project"`
-	Networks  []string  `json:"networks"`
-	Services  []Service `json:"services"`
+	Version     string    `json:"version"`
+	CreatedAt   time.Time `json:"created_at"`
+	Project     string    `json:"project"`
+	Networks    []string  `json:"networks"`
+	Services    []Service `json:"services"`
 	Incremental bool      `json:"incremental"`
-	BasePack     string    `json:"base_pack"`
+	BasePack    string    `json:"base_pack"`
 }
 
 type LastManifest struct {
 	PackageHash string          `json:"package_hash"`
-	Manifest   PackageManifest `json:"manifest"`
+	Manifest    PackageManifest `json:"manifest"`
 }
 
 type MountInfo struct {
 	Type        string `json:"type"`
-	Source     string `json:"source"`
+	Source      string `json:"source"`
 	Destination string `json:"destination"`
-	ReadOnly   bool   `json:"read_only"`
+	ReadOnly    bool   `json:"read_only"`
 }
 
 type ContainerMounts struct {
-	ContainerName string     `json:"container_name"`
-	Mounts       []MountInfo `json:"mounts"`
+	ContainerName string      `json:"container_name"`
+	Mounts        []MountInfo `json:"mounts"`
 }

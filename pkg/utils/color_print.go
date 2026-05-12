950cf1081fd984ccaee11bdf2fbcf77535645b6d
Ray-D-Song <songraysmail@gmail.com>
2026-05-12 13:36:46 +0800

    chore: slient flag for print

--- pkg/utils/color_print.go
+++ pkg/utils/color_print.go
@@ -2,6 +2,8 @@ package utils
 
 import "fmt"
 
+var slient = false
+
 const (
 	blue   = "\033[34m"
 	green  = "\033[32m"
@@ -18,17 +20,29 @@ const (
 )
 
 func PrintI(f string, a ...any) {
-	fmt.Printf(blue+f+reset, a...)
+	if !slient {
+		fmt.Printf(blue+f+reset, a...)
+	}
 }
 
 func PrintS(f string, a ...any) {
-	fmt.Printf(green+f+reset, a...)
+	if !slient {
+		fmt.Printf(green+f+reset, a...)
+	}
 }
 
 func PrintW(f string, a ...any) {
-	fmt.Printf(yellow+f+reset, a...)
+	if !slient {
+		fmt.Printf(yellow+f+reset, a...)
+	}
 }
 
 func PrintE(f string, a ...any) {
-	fmt.Printf(red+f+reset, a...)
+	if !slient {
+		fmt.Printf(red+f+reset, a...)
+	}
+}
+
+func SetSlient() {
+	slient = true
 }
